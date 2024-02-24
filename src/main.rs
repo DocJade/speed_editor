eeacae823ca7cab397c4589af562e19f5336003c
DocJade Desktop <ytdocjade@gmail.com>
2024-02-24 15:52:26 -0700

    Jog modes

--- src/main.rs
+++ src/main.rs
@@ -102,6 +102,13 @@ enum SpeedEditorJogLed {
 	Scrl		= (1 <<  2),
 }
 
+enum SpeedEditorJogMode {
+    RELATIVE_0			= 0,		// # Rela
+	ABSOLUTE_CONTINUOUS	= 1,		// # Send an "absolute" position (based on the position when mode was set) -4096 -> 4096 range ~ half a turn
+	RELATIVE_2			= 2,		// # Same as mode 0 ?
+	ABSOLUTE_DEADZERO	= 3,		// # Same as mode 1 but with a small dead band around zero that maps to 0
+}
+
 fn main() {
     println!("Hello, world!");
 }
